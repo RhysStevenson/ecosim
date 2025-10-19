06a0598c7b8261dd5391a01f55a666bd1649cf40
Rhys Stevenson <stevensonrs@hotmail.co.uk>
2025-10-19 22:04:40 +0100

    cleanup

--- ecosim/src/simulationController.ts
+++ ecosim/src/simulationController.ts
@@ -2,7 +2,6 @@
 export class SimulationController {
   private running = true;
   private speed = 1;
-  private foodSpawnInterval = 0.1; // seconds
 
   togglePause() {
     this.running = !this.running;
